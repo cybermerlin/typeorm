121c6d9e758f7ff85ce5f971921a76a8f49b940e
cybermerlin <cybermerlin@ya.ru>
2018-05-28 18:42:05 +0300

    remove useless

--- test/other-issues/lateral-join/entity/NeighborPos.ts
+++
@@ -1,8 +0,0 @@
-import {Column, Entity} from "../../../../src";
-import Base from "./Base";
-
-@Entity("neighbor_pos")
-export default class NeighborPos extends Base {
-    @Column()
-    id_unit: number;
-}
